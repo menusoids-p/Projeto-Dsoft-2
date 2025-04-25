cf5fca309d5b4285bb101f1c6799f6901fb3e21a
laptop <felipecm13@al.insper.com.br>
2025-04-25 12:52:29 -0300

    laptop. 2 e 3

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,21 @@ def rolar_dados(n):
         x = random.randint(1,6)
         lista.append(x)
     return lista
+
+def guardar_dado(rolados,guardados,indice):
+    valor = rolados[indice]
+    guardados.append(valor)
+
+    del(rolados[indice])
+
+    return [rolados, guardados]
+
+def remover_dado(rolados,guardados,indice):
+    valor = guardados[indice]
+    rolados.append(valor)
+
+    del(guardados[indice])
+
+    return [rolados, guardados]
+
+
